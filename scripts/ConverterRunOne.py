ddf4069dbac2354dd93c792fd69b77b7f9c81d69
Pablo De Castro Manzano <pablodecm@gmail.com>
2015-06-17 14:57:30 -0400

    Add Tbar samples

--- scripts/ConverterRunOne.py
+++ scripts/ConverterRunOne.py
@@ -6,7 +6,9 @@ from os.path import split
 
 rootfiles = [ "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/TTbar_Summer13.root", 
               "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/T_t_Summer13.root",
+              "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/Tbar_t_Summer13.root",
               "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/T_tW_Summer13.root",
+              "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/Tbar_tW_Summer13.root",
               "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/WJets_Summer13.root",
               "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/ZJets_Summer13.root",
               "/user_data/pdecastr/BTagSF/Samples/8TeV/53x_Summer13/Data_2012ABCD_Winter13_ReReco.root"]
